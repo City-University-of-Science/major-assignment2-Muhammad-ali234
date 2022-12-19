4ccda7a9d4817158431f84356b1ad9a7af6573f9
Muhammad-ali234 <muhammadali.chd12@gmail.com>
2022-12-19 19:24:25 +0500

    Adding condition(sum,sub,mult,Divis)

--- Scientific Calculator/main.dart
+++ Scientific Calculator/main.dart
@@ -14,4 +14,24 @@ void main() {
   //decision input from user for calculation
   print("Enter the operation (+, -, *, /, sqrt, sin, cos, tan):");
   String operation = stdin.readLineSync()!;
+
+//put if else condition to print specific condition on specific time
+  double? outPutResult;
+  //condition for addition
+  if (operation == "+") {
+    outPutResult = num1 + num2;
+  }
+  //condition for subtraction
+  else if (operation == "-") {
+    outPutResult = num1 - num2;
+  }
+  //condition for Multiplication
+  else if (operation == "*") {
+    outPutResult = num1 * num2;
+  }
+  //condition for division
+  else if (operation == "/") {
+    outPutResult = num1 / num2;
+  }
+  print("The result is: $outPutResult");
 }

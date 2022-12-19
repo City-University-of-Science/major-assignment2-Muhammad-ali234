fd13935ae265b77ed697904ab3d183c18ad63853
Muhammad-ali234 <muhammadali.chd12@gmail.com>
2022-12-19 19:09:28 +0500

    Adding two Input for users

--- Scientific Calculator/main.dart
+++ Scientific Calculator/main.dart
@@ -1,3 +1,17 @@
-void main(List<String> args) {
-  print('scientific calculator');
+import 'dart:io';
+
+void main() {
+  print("Welcome to the scientific calculator!");
+
+  //Taking input from user 1st input
+  print("Enter the first number:");
+  double num1 = double.parse(stdin.readLineSync()!);
+
+  //Taking input from user 2st input
+  print("Enter the second number (if applicable):");
+  double num2 = double.parse(stdin.readLineSync()!);
+
+  //decision input from user for calculation
+  print("Enter the operation (+, -, *, /, sqrt, sin, cos, tan):");
+  String operation = stdin.readLineSync()!;
 }
